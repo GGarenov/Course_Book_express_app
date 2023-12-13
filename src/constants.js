00a82531661a0e140a93906f69a0815352aae1c7
Georgiy Garenov <g_garenov@outlook.com>
2023-12-13 21:04:03 +0200

    Config file for the database, SECRET and PORT

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  PORT: 3000,
+  DB_URL: "mongodb://127.0.0.1:27017/course_book", //localhost = 127.0.0.1
+  SECRET: "3472cce4-e305-4b3d-8a75-ab6ddfd884b2",
+};
+
+module.exports = CONFIG;
